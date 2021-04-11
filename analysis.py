4a4436ec4cb7b3159eae343773222ed92c5cb241
DracoNibilis <mniamM@gmail.com>
2021-04-11 11:27:37 +0100

    histo

--- analysis.py
+++ analysis.py
@@ -2,41 +2,27 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+# reading data from csv file as a pandas data frame with set index as a class
+frame = pd.read_csv("iris_csv.csv", index_col="class")
+#rename index
+frame.index.name = "IRIS CLASS"
+# grouping index into 3 groups ( flower names) - making multiindex
+f1 = frame.index.to_series()
+f2 = f1.groupby(f1).cumcount()
+frame.index = [frame.index, f2]
 
-frame = pd.read_csv("iris_csv.csv")
-#print(frame.head())
-#print(frame.describe())
-test_frame = frame.sort_values(by="class")
-#test_frame.rename(columns = {"class": "flower_class"})
-#print(test_frame.head())
-#print(test_frame.head())
-#change index for "class"
-#class_index = pd.read_csv("iris_csv.csv", index_col="class")
-#print(class_index.tail(20))
+#making variables by multiindex - 3 variables by flowers
+irises = frame.groupby(by=["IRIS CLASS"]) # grouping data by index ( 3 types )
+#print(irises.size())
+#get group - making variables for separate iris types (3 types)
+iris_setosa = irises.get_group("Iris-setosa")
+iris_versicolor = irises.get_group("Iris-versicolor")
+iris_virginica = irises.get_group("Iris-virginica")
+#print(iris_setosa.head())
+#print(type(iris_setosa))
 
-#checking index and its value
-#print(list(class_index.index))
-
-#grupby data by variables ( 3 types of flowers) , convert column to type category
-#print(test_frame['flower_class'].value_counts())
-#declare variables for unique flower types 
-irises = test_frame['class'].unique()
-#print(irises)
-flower_groups = {} # empty dictionary 
-for flower in irises:
-    sub_flower = test_frame.where(test_frame['class'] == flower).dropna()
-    flower_groups[flower] = sub_flower
-#print(flower_groups.keys())
-#print(flower_groups['Iris-virginica'].head())
-
-#making plot ( histogram ) for each variable
-var_1 = pd.DataFrame(flower_groups['Iris-setosa'])
-print(var_1)
-var_1_n = np.array(var_1)
-'''print(var_1_n)
-
-plt.title("iris setosa")
-plt.plot(var_1_n)
-
-plt.legend()
-plt.show()'''
+#making histogram for each variables 
+plt.title("IRIS SETOSA") # title
+plt.figure()
+iris_setosa.plot.hist() # basic histogram for first variable
+plt.show()
